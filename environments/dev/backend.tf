0f490fe9dd0e91d697b7b43525d9676f24cae47d
Bose Karthikeyan <bkarth17@ford.com>
2021-02-20 22:45:49 +0530

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "cloudops-sandbox-302521-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="cloudops-sandbox-302521"
